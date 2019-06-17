8b79aa6b455d27b2379cfd0bed4190c3c0f4be38
Abeer Khalaf <abeerkhalaf96@gmail.com>
2019-06-17 13:19:23 -0400

    official

--- java.js
+++ java.js
@@ -7,3 +7,11 @@ Holder.addTheme('gray', {
     font: 'Helvetica',
     fontweight: 'normal'
 });
+
+var vglnk = {key: 'aa20b4f1a9ca752cb20f3ab70d55dee7'};
+(function(d, t) {
+    var s = d.createElement(t);s.type = 'text/javascript';
+    s.async = true;s.src = '//cdn.viglink.com/api/vglnk.js';
+    var r = d.getElementsByTagName(t)[0];
+    r.parentNode.insertBefore(s, r);
+}(document, 'script'));
