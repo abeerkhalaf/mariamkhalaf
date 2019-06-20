003f1592a0c88d5b55185e4c20877561e56438a8
Abeer Khalaf <abeerkhalaf96@gmail.com>
2019-06-20 17:02:26 -0400

    official

--- public/scripts.js
+++ public/scripts.js
@@ -15,3 +15,28 @@ var vglnk = {key: 'aa20b4f1a9ca752cb20f3ab70d55dee7'};
     var r = d.getElementsByTagName(t)[0];
     r.parentNode.insertBefore(s, r);
 }(document, 'script'));
+
+document.getElementById("submit-btn").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+const myform = document.getElementById('myform');
+
+myform.addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+    const formData = new FormData(this);
+
+    fetch('contact-form.php',{
+        method:'post',
+        body: formData
+    }).then(function(response){
+        return response.text();
+    }).then(function (text){
+        console.log(text);
+    }).catch(function (error) {
+        console.error(error);
+    })
+});
+
